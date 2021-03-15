7ac82418ce6bd4415ac23b9dd96c1b1ab800967a
hakimbalestrieri <hakim.balestrieri@travizory.com>
2021-03-15 22:33:33 +0100

    fix FileTransformerTest issue

--- LabJavaIO/src/main/java/ch/heigvd/res/labio/impl/transformers/FileTransformer.java
+++ LabJavaIO/src/main/java/ch/heigvd/res/labio/impl/transformers/FileTransformer.java
@@ -13,55 +13,55 @@ import java.util.logging.Logger;
  * This abstract class implements the IFileVisitor interface and has the responsibility
  * to open an input text file, to read its content, to apply a number of transformations
  * (via filters) and to write the result in an output text file.
- * 
+ * <p>
  * The subclasses have to implement the decorateWithFilters method, which instantiates
  * a list of filters and decorates the output writer with them.
- * 
+ *
  * @author Olivier Liechti
  */
 public abstract class FileTransformer implements IFileVisitor {
 
-  private static final Logger LOG = Logger.getLogger(FileTransformer.class.getName());
-  private final List<FilterWriter> filters = new ArrayList<>();
-  
-  /**
-   * The subclasses implement this method to define what transformation(s) are
-   * applied when writing characters to the output writer. The visit(File file)
-   * method creates an output file and creates a corresponding writer. It then
-   * calls decorateWithFilters and passes the writer as argument. The method
-   * wraps 0, 1 or more filter writers around the original writer and returns 
-   * the result.
-   * 
-   * @param writer the writer connected to the output file
-   * @return the writer decorated by 0, 1 or more filter writers
-   */
-  public abstract Writer decorateWithFilters(Writer writer);
+    private static final Logger LOG = Logger.getLogger(FileTransformer.class.getName());
+    private final List<FilterWriter> filters = new ArrayList<>();
 
-  @Override
-  public void visit(File file) {
-    if (!file.isFile()) {
-      return;
+    /**
+     * The subclasses implement this method to define what transformation(s) are
+     * applied when writing characters to the output writer. The visit(File file)
+     * method creates an output file and creates a corresponding writer. It then
+     * calls decorateWithFilters and passes the writer as argument. The method
+     * wraps 0, 1 or more filter writers around the original writer and returns
+     * the result.
+     *
+     * @param writer the writer connected to the output file
+     * @return the writer decorated by 0, 1 or more filter writers
+     */
+    public abstract Writer decorateWithFilters(Writer writer);
+
+    @Override
+    public void visit(File file) {
+        if (!file.isFile()) {
+            return;
+        }
+        try {
+            Reader reader = new InputStreamReader(new FileInputStream(file), StandardCharsets.UTF_8);
+            Writer writer = new OutputStreamWriter(new FileOutputStream(file.getPath() + ".out"), StandardCharsets.UTF_8); // the bug fix by teacher
+            writer = decorateWithFilters(writer);
+
+            BufferedReader br = new BufferedReader(reader);
+            BufferedWriter bw = new BufferedWriter(writer);
+
+            int index = br.read();
+            while (index != -1) {
+                bw.write(index);
+                index = br.read();
+            }
+
+            br.close();
+            bw.flush();
+            bw.close();
+        } catch (IOException ex) {
+            LOG.log(Level.SEVERE, null, ex);
+        }
     }
-    try {
-      Reader reader = new InputStreamReader(new FileInputStream(file), StandardCharsets.UTF_8);
-      Writer writer = new OutputStreamWriter(new FileOutputStream(file.getPath()+ ".out"), StandardCharsets.UTF_8); // the bug fix by teacher
-      writer = decorateWithFilters(writer);
-
-      BufferedReader br = new BufferedReader(reader);
-      BufferedWriter bw = new BufferedWriter(writer);
-
-      int index = br.read();
-      while (index != -1) {
-        bw.write(index);
-        index = br.read();
-      }
-      
-      reader.close();
-      writer.flush();
-      writer.close();
-    } catch (IOException ex) {
-      LOG.log(Level.SEVERE, null, ex);
-    }
-  }
 
 }
