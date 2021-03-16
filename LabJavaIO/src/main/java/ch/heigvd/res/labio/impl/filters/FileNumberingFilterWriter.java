6c8b1081478cb9990764776275c7f22736fc17a2
Delphine Scherler <delphine.scherler@heig-vd.ch>
2021-03-16 13:44:46 +0100

    Implement Numbering filter

--- LabJavaIO/src/main/java/ch/heigvd/res/labio/impl/filters/FileNumberingFilterWriter.java
+++ LabJavaIO/src/main/java/ch/heigvd/res/labio/impl/filters/FileNumberingFilterWriter.java
@@ -1,5 +1,7 @@
 package ch.heigvd.res.labio.impl.filters;
 
+import ch.heigvd.res.labio.impl.Utils;
+
 import java.io.FilterWriter;
 import java.io.IOException;
 import java.io.Writer;
@@ -13,29 +15,64 @@ import java.util.logging.Logger;
  *
  * Hello\n\World -> 1\Hello\n2\tWorld
  *
- * @author Olivier Liechti
+ * @author Delphine Scherler
  */
 public class FileNumberingFilterWriter extends FilterWriter {
 
   private static final Logger LOG = Logger.getLogger(FileNumberingFilterWriter.class.getName());
 
+  private int lineNo = 0;
+  private int lastChar;
+
   public FileNumberingFilterWriter(Writer out) {
     super(out);
   }
 
+  private void writeLine() throws IOException {
+    String s = ++lineNo + "\t";
+    super.write(s, 0, s.length());
+  }
+
   @Override
   public void write(String str, int off, int len) throws IOException {
-    throw new UnsupportedOperationException("The student has not implemented this method yet.");
+
+    String[] strOut = Utils.getNextLine(str.substring(off, off + len));
+
+    if(lineNo == 0){
+      writeLine();
+    }
+
+    while(!strOut[0].isEmpty()){
+      super.write(strOut[0], 0, strOut[0].length());
+      writeLine();
+      strOut = Utils.getNextLine(strOut[1]);
+    }
+
+    super.write(strOut[1], 0, strOut[1].length());
   }
 
   @Override
   public void write(char[] cbuf, int off, int len) throws IOException {
-    throw new UnsupportedOperationException("The student has not implemented this method yet.");
+    for(int i = off; i < len; i++){
+      write(cbuf[i]);
+    }
   }
 
   @Override
   public void write(int c) throws IOException {
-    throw new UnsupportedOperationException("The student has not implemented this method yet.");
+
+    if(lineNo == 0){
+      writeLine();
+    }
+
+    super.write(c);
+
+    //Handle write with an int
+    if(lastChar == '\r' || c == '\n'){
+      writeLine();
+    }
+
+    lastChar = c;
   }
 
 }
