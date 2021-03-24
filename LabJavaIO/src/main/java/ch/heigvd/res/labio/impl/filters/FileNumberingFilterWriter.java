33c1408f0920dcfade237d46c615b1f71c28aefd
Corentin ZELLER <junk@corentin-zeller.ch>
2021-03-24 10:15:18 +0100

    FileNumberingFilterWriter works on other than mac too now (yikes)

--- LabJavaIO/src/main/java/ch/heigvd/res/labio/impl/filters/FileNumberingFilterWriter.java
+++ LabJavaIO/src/main/java/ch/heigvd/res/labio/impl/filters/FileNumberingFilterWriter.java
@@ -18,6 +18,8 @@ import java.util.logging.Logger;
 public class FileNumberingFilterWriter extends FilterWriter {
 
   private static final Logger LOG = Logger.getLogger(FileNumberingFilterWriter.class.getName());
+  private int lineCount = 0;
+  private int previousCharProcessed;
 
   public FileNumberingFilterWriter(Writer out) {
     super(out);
@@ -25,17 +27,37 @@ public class FileNumberingFilterWriter extends FilterWriter {
 
   @Override
   public void write(String str, int off, int len) throws IOException {
-    throw new UnsupportedOperationException("The student has not implemented this method yet.");
+    for(int i = off; i < off + len; ++i) {
+      write(str.charAt(i));
+    }
   }
 
   @Override
   public void write(char[] cbuf, int off, int len) throws IOException {
-    throw new UnsupportedOperationException("The student has not implemented this method yet.");
+    for(int i = off; i < off + len; ++i) {
+      write(cbuf[i]);
+    }
   }
 
   @Override
   public void write(int c) throws IOException {
-    throw new UnsupportedOperationException("The student has not implemented this method yet.");
+
+    if(previousCharProcessed == '\r' && c != '\n' || lineCount == 0) {
+      out.write(getLinePrefix());
+    }
+
+    out.write(c);
+
+    if(c == '\n') {
+      out.write(getLinePrefix());
+    }
+
+    previousCharProcessed = c;
+  }
+
+  private String getLinePrefix() {
+    lineCount++;
+    return lineCount + "\t";
   }
 
 }
