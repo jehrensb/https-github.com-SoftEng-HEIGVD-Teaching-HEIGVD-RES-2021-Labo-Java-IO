bc0948206e306b3939b530adaaf4faad04dcf0ab
AloisChristen <alois.christen@heig-vd.ch>
2021-03-25 14:13:30 +0100

    Passed all UpperCaseWriter tests

--- LabJavaIO/src/main/java/ch/heigvd/res/labio/impl/filters/UpperCaseFilterWriter.java
+++ LabJavaIO/src/main/java/ch/heigvd/res/labio/impl/filters/UpperCaseFilterWriter.java
@@ -3,6 +3,9 @@ package ch.heigvd.res.labio.impl.filters;
 import java.io.FilterWriter;
 import java.io.IOException;
 import java.io.Writer;
+import java.util.Locale;
+
+import static java.lang.Character.toUpperCase;
 
 /**
  *
@@ -16,17 +19,23 @@ public class UpperCaseFilterWriter extends FilterWriter {
 
   @Override
   public void write(String str, int off, int len) throws IOException {
-    out.write(str, off, len);
+    String output = str.substring(0, off) +
+            str.substring(off, off + len).toUpperCase(Locale.ROOT) +
+            str.substring(off + len);
+    out.write(output, off, len);
   }
 
   @Override
   public void write(char[] cbuf, int off, int len) throws IOException {
+    for(int i = off; i < off + len; i++){
+      cbuf[i] = toUpperCase(cbuf[i]);
+    }
     out.write(cbuf, off, len);
   }
 
   @Override
   public void write(int c) throws IOException {
-    out.write(c);
+    out.write(toUpperCase(c));
   }
 
 }
